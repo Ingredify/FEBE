9173d29afc8eeb787a0409bced2df0797ac69c9b
Shinzi <shinzisaja@gmail.com>
2025-06-10 22:07:38 +0700

    reset migration, changing to cloud database, fixing dockerfile

--- Ingredify-Back-End/src/server.js
+++ Ingredify-Back-End/src/server.js
@@ -5,8 +5,8 @@ const routes = require('./routes/routes')
 const authPlugin = require('./plugins/auth');
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
   });
 
   await server.register(authPlugin);
